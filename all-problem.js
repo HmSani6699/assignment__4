7654415b0c599cec62bde9f719a64e3f119488cc
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-07 10:11:07 +0600

    ad to assignment problem 1

--- all-problem.js
+++ all-problem.js
@@ -1,51 +1,62 @@
+/* 
+This function will take a positive number as input and multiply that number by 3.
+Then add 10, then divide by 2,
+Then after subtracting 5, the output will be returned
+*/
 function mindGame(number) {
     if (typeof number !== 'number') {
-        return 'Please Provide Your Number';
+        return 'Please Provide a number!';
     }
-    return (number * 3 + 10) / 2 - 5
+    return (number * 3 + 10) / 2 - 5;
 }
-// console.log(mindGame('33'))
 
-//------------------------------22222-------------------//
 
+/* 
+This function will take a string as input and check the total number of characters in the string
+If the number of total character is positive then 'even' will return positive otherwise 'odd'
+*/
 function evenOdd(name) {
     if (typeof name !== 'string') {
-        return 'Please Provide Your Text';
+        return 'Please provide a text!!';
     }
 
     if (name.length % 2 === 0) {
         return 'even'
     }
-    else {
-        return 'odd'
-    }
+    return 'odd'
 }
-// console.log(evenOdd('chatgpt'))
+// console.log(evenOdd('chatgt'))
 
 
 //-----------------------------3333-------------------//
 
+/* 
+This function will take a number as input and subtract seven from that number, then if the subtraction is less than seven, then it will return the subtraction, and if it is greater than seven, it will return double the input.
+*/
 function isLGSeven(number) {
     if (typeof number !== 'number') {
-        return 'Please Provide Your Number';
+        return 'Please Provide a number!';
     }
 
     const result = number - 7;
     if (result < 7) {
         return result;
     }
-    else {
-        return number * 2;
-    }
-}
+    return number * 2;
 
+}
 // console.log(isLGSeven(6))
 
 //---------------------------44444--------------------------//
 
+/* 
+This function will take an array as input then the number of bad data in the array
+ It will find it and return that number
+*/
+
 function findingBadData(array) {
     if (Array.isArray(array) !== true) {
-        return 'Please Provide Your Array'
+        return 'Please Provide An Array'
     }
     let negativeData = 0;
     for (let i = 0; i < array.length; i++) {
@@ -56,27 +67,28 @@ function findingBadData(array) {
     }
     return negativeData
 }
-// console.log(findingBadData([-4, -9, -5, -33, -55]))
+// console.log(findingBadData([-4, -9, -5, 33, -55]))
 
 
 //-----------------------------5555----------------------------//
 
+/* 
+This function will take three parameters, parameter three must be a number, otherwise it will show an error, it will multiply each number by a specific number, then add the product together, if the sum is greater than 2000, subtract 2000 from the sum and return the minus, otherwise, the previous sum will return a tie.
+*/
+
 function gemsToDiamond(num1, num2, num3) {
-    if (typeof num1 === 'number' && typeof num2 === 'number' && typeof num3 === 'number') {
-        let firstFriends = num1 * 21;
-        let secondFriends = num2 * 32;
-        let thirdFriends = num3 * 43;
-
-        let totalDiamond = firstFriends + secondFriends + thirdFriends;
-        if (totalDiamond > 2000) {
-            return totalDiamond - 2000
-        }
-        return totalDiamond
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') {
+        return ("Please provide a number!")
     }
-    else {
-        return 'Please Provide Your Number'
-    }
+    let firstFriends = num1 * 21;
+    let secondFriends = num2 * 32;
+    let thirdFriends = num3 * 43;
 
+    let totalDiamond = firstFriends + secondFriends + thirdFriends;
+    if (totalDiamond > 2000) {
+        return totalDiamond - 2000
+    }
+    return totalDiamond
 }
 // console.log(gemsToDiamond(20, 200, 50))
 
--- assignment.js
+++ assignment.js
@@ -6,9 +6,21 @@
  তারপর 5 বিয়োগ করে, যা আউটপুট আসবে তা return করতে হবে। Sample Input & Output:-
 */
 
+
+/* 
+
+এই function টি  ইনপুট হিসেবে একটা positive number নিবে  সেই নাম্বার এর সাথে 3 গুন করে,
+ তারপর 10 যোগ করে, তারপর 2 দিয়ে ভাগ করে,
+ তারপর 5 বিয়োগ করে,যা আউটপুট আসবে তা return করেছি
+
+This function will take a positive number as input and multiply that number by 3.
+Then add 10, then divide by 2,
+Then after subtracting 5, the output will be returned
+*/
+
 function mindGame(number) {
     if (typeof number !== 'number') {
-        return 'Please Provide Your Number';
+        return 'Please Provide a number!';
     }
     return (number * 3 + 10) / 2 - 5
 }
@@ -22,6 +34,13 @@ function mindGame(number) {
 এখন তোমার task: String এ total কয়টা character আছে তার উপর ভিত্তি করে আউটপুট দিতে হবে। 
 আউটপুট হবে ‘Even’ অথবা ‘Odd’ ।
 
+
+এই function  ইনপুট হিসেবে একটা string নিবে   String এ total কয়টা character আছে তা চেক করবে
+total character এর সংখ্যা পজিটিভ হলে ‘even’ পজিটিভ নাহলে ‘odd’  return করবে
+
+This function will take a string as input and check the total number of characters in the string
+If the number of total character is positive then 'even' will return positive otherwise 'odd'
+
 */
 
 function evenOdd(name) {
@@ -32,11 +51,9 @@ function evenOdd(name) {
     if (name.length % 2 === 0) {
         return 'even'
     }
-    else {
-        return 'odd'
-    }
+    return 'odd'
 }
-// console.log(evenOdd('chatgpt'))
+console.log(evenOdd('chatgt'))
 
 
 
@@ -45,23 +62,28 @@ function evenOdd(name) {
 তোমাকে একটা function দেওয়া হবে called “isLGSeven()”. এটা ইনপুট হিসেবে একটা number নিবে।
 
 এখন তোমার task: তোমাকে ইনপুট ভ্যালু এবং ৭ এর মধ্যে পার্থক্য বের করতে হবে। যদি এই পার্থক্য ৭ এর চেয়ে ছোট হয়, তখন তোমাকে return করতে হবে সেই বিয়োগফল। নাহলে তোমাকে return করতে হবে double of the input। মানে যে সংখ্যা ইনপুট হিসেবে নিয়েছো সেটির দ্বিগুণ।
+
+
+
+এই function টি ইনপুট হিসেবে একটা number নিবে  ওই নাম্বারের সাথে সাত বিয়োগ করবে তারপর বিয়োগফল যদি সাতের ছোট হয় তাহলে বিয়োগফলটা রিটার্ন করবে আর সাতের বড় হলে ইনপুট যেটা নিয়েছি তার দ্বিগুণ রিটার্ন করবে
+
+This function will take a number as input and subtract seven from that number, then if the subtraction is less than seven, then it will return the subtraction, and if it is greater than seven, it will return double the input.
 */
 
 function isLGSeven(number) {
     if (typeof number !== 'number') {
-        return 'Please Provide Your Number';
+        return 'Please Provide a number!';
     }
 
     const result = number - 7;
     if (result < 7) {
         return result;
     }
-    else {
-        return number * 2;
-    }
+    return number * 2;
+
 }
 
-// console.log(isLGSeven(6))
+console.log(isLGSeven(19))
 
 
 /*-----------------------Problem 4: Finding Bad data-----------------------//
@@ -69,13 +91,19 @@ function isLGSeven(number) {
 তোমাকে একটা function দেওয়া হবে called findingBadData()”. এটা ইনপুট হিসেবে একটা array নিবে। array তে 
 যেকোনো সংখ্যক কতগুলো number থাকবে। number গুলো negative number (less than zero) ও হতে পারে, আবার positive 
 number (greater than or equal to zero) ও হতে পারে। কোনও number যদি negative হয় সেটাকে আমরা বলব “Bad Data”.
- কোনও number যদি positive হয় সেটাকে আমরা বলব “Good Data”. এখন তোমার task: array তে কতগুলো Bad data আছে
+ কোনও number যদি positive হয় সেটাকে আমরা বলব “Good Data”. এখন তোমার task: array তে যতগুলো Bad data আছে
  সেটা খুঁজে বের করতে হবে এবং সেই নাম্বার টা return করতে হবে।
+
+ এই ফাংশনের কাজ হচ্ছে ইনপুট প্যারামিটার হিসেবে আমরা একটা array নিয়েছি  array তে যতগুলো Bad data আছে
+ সেটা খুঁজে বের করেছি এবং সেই নাম্বার টা return করেছি।
+
+ The function of this function is as input parameter we have taken an array as many bad data in the array
+ I found it and returned that number.
 */
 
 function findingBadData(array) {
     if (Array.isArray(array) !== true) {
-        return 'Please Provide Your Array'
+        return 'Please Provide An Array'
     }
     let negativeData = 0;
     for (let i = 0; i < array.length; i++) {
@@ -86,7 +114,7 @@ function findingBadData(array) {
     }
     return negativeData
 }
-// console.log(findingBadData([-4, -9, -5, -33, -55]))
+console.log(findingBadData([-4, -9, -5, 33, -55]))
 
 
 /*-------------------------Problem 5: Convert your gems into diamond--------------------------//
@@ -101,23 +129,23 @@ function findingBadData(array) {
 
 [ gems এর ক্ষমতা বলতে একটা gems কে কয়টা diamond এ convert করা যাবে সেটা বুঝানো হচ্ছে। এখন তোমার task: সব বন্ধুর gems মিলিয়ে total কতটি diamond পাবে সেটা বের করতে হবে। total diamond এর সংখ্যা যদি 1000 এর দিগুন এর উপর হয়ে যায় সেক্ষেত্রে total diamond থেকে 2000 বাদ দিয়ে যতগুলো diamond অবশিষ্ট থাকবে, ততগুলো তোমরা পাবে।Sample Input & Output:-
 
+This function will take three parameters, parameter three must be a number, otherwise it will show an error, it will multiply each number by a specific number, then add the product together, if the sum is greater than 2000, subtract 2000 from the sum and return the minus, otherwise, the previous sum will return a tie.
+
 */
 
+
 function gemsToDiamond(num1, num2, num3) {
-    if (typeof num1 === 'number' && typeof num2 === 'number' && typeof num3 === 'number') {
-        let firstFriends = num1 * 21;
-        let secondFriends = num2 * 32;
-        let thirdFriends = num3 * 43;
-
-        let totalDiamond = firstFriends + secondFriends + thirdFriends;
-        if (totalDiamond > 2000) {
-            return totalDiamond - 2000
-        }
-        return totalDiamond
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') {
+        return ("Please provide a number!")
     }
-    else {
-        return 'Please Provide Your Number'
-    }
+    let firstFriends = num1 * 21;
+    let secondFriends = num2 * 32;
+    let thirdFriends = num3 * 43;
 
+    let totalDiamond = firstFriends + secondFriends + thirdFriends;
+    if (totalDiamond > 2000) {
+        return totalDiamond - 2000
+    }
+    return totalDiamond
 }
-// console.log(gemsToDiamond(20, 200, 50))
+console.log(gemsToDiamond(20, 200, 50))
